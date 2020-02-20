dc3d9be4e02a6bb98555839cfeb7223edf6aab33
Cédric Matyja <c.matyja@gmail.com>
2020-02-20 16:47:51 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-PasswordGenerator';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    if (isNaN(parseInt(value)) || parseInt(value) > 99 || parseInt(value) < 0) {
+      this.length = 0;
+    } else {
+      this.length = parseInt(value);
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const NUMBERS = '0123456789';
+    const LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+    const SYMBOLS = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += LETTERS;
+    }
+    if (this.includeNumbers) {
+      validChars += NUMBERS;
+    }
+    if (this.includeSymbols) {
+      validChars += SYMBOLS;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  copyInputMessage(inputElement){
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+  }
 }
